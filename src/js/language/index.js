be5b0f36464770395ffa2bd161ae9cce7e50569f
Vahan Hovhannisyan <vaa.hovanisyan@gmail.com>
2024-07-27 07:30:27 +0400

    first commit

---
+++ src/js/language/index.js
@@ -0,0 +1,117 @@
+// import { langList } from "./langList.js";
+import { URL } from "../index.js";
+const buttonRu = document.querySelectorAll(".language__button--ru");
+const buttonEng = document.querySelectorAll(".language__button--eng");
+
+let currentLanguage =
+	localStorage.getItem("language") ||
+	window.location.hash.substring(1) ||
+	window.navigator.language.substring(0, 2) ||
+	"en";
+
+function changeLanguage(lang) {
+	const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				fetch(
+					`${URL}/api/${entry.target.dataset.langs}?populate=*&locale=${lang}`
+				)
+					.then((res) => res.json())
+					.then(({ data }) => {
+						if (entry.target.dataset.langs === "header") {
+							const navList = document.querySelector(".nav__list");
+							navList.innerHTML = "";
+							data.links.forEach((link) => {
+								const li = document.createElement("li");
+								li.classList.add("nav__item");
+								li.innerHTML = `<a class="nav__link link" href="${link.link}">${link.text}</a>`;
+								navList.appendChild(li);
+							});
+						}
+
+						if (entry.target.dataset.langs === "hero") {
+							const name = document.querySelector(".hero__name");
+							name.textContent = data.name;
+							const lastname = document.querySelector(".hero__lastname");
+							lastname.textContent = data.lastname;
+							const text = document.querySelector(".hero__text");
+							text.innerHTML = data.text;
+						}
+
+						if (entry.target.dataset.langs === "about") {
+							const title = document.querySelector(".about__title");
+							title.innerHTML = data.title;
+							const text1 = document.querySelector(".about__text");
+							text1.innerHTML = data.text;
+						}
+
+						if (entry.target.dataset.langs === "portfolio") {
+							const title = document.querySelector(".portfolio__title");
+							title.textContent = data.title;
+							data.portfolioitems.forEach((item, index) => {
+								document.querySelectorAll(".portfolio__slide-link")[
+									index
+								].textContent = item.title;
+							});
+						}
+
+						if (entry.target.dataset.langs === "skill") {
+							const title = document.querySelector(".skills__title");
+							title.textContent = data.title;
+							const text = document.querySelector(".skills__text");
+							text.innerHTML = data.text;
+
+							data.skillitems.forEach((item, index) => {
+								document.querySelectorAll(".skills__item-title")[
+									index
+								].textContent = item.title;
+							});
+						}
+
+						if (entry.target.dataset.langs === "contact") {
+							const title = document.querySelector(".contacts__title");
+							title.textContent = data.title;
+							const text = document.querySelector(".contacts__text");
+							text.innerHTML = data.text;
+							const button = document.querySelector(".contacts__button");
+							button.textContent = data.buttontext;
+						}
+
+						if (entry.target.dataset.langs === "footer") {
+							const text = document.querySelector(".footer__text");
+							text.innerHTML = data.text;
+						}
+					});
+
+				observer.unobserve(entry.target);
+			}
+		});
+	});
+	localStorage.setItem("language", lang);
+	window.location.hash = lang;
+	document.querySelectorAll("[data-langs]").forEach((item) => {
+		observer.observe(item);
+	});
+}
+
+changeLanguage(currentLanguage);
+
+buttonRu.forEach((button, index) => {
+	button.classList.toggle("language__button_active", currentLanguage === "ru");
+
+	button.addEventListener("click", () => {
+		buttonEng[index].classList.remove("language__button_active");
+		button.classList.add("language__button_active");
+		changeLanguage("ru");
+	});
+});
+
+buttonEng.forEach((button, index) => {
+	button.classList.toggle("language__button_active", currentLanguage === "en");
+
+	button.addEventListener("click", () => {
+		buttonRu[index].classList.remove("language__button_active");
+		button.classList.add("language__button_active");
+		changeLanguage("en");
+	});
+});
---
+++ src/js/language/langList.js
@@ -0,0 +1,148 @@
+export const langList = {
+	header: {
+		home: {
+			ru: "Главная",
+			en: "Home",
+		},
+		about: {
+			ru: "Обо мне",
+			en: "Aboute me",
+		},
+		skills: {
+			ru: "Навыки",
+			en: "Skills",
+		},
+		portfolio: {
+			ru: "Портфолио",
+			en: "Portfolio",
+		},
+		contacts: {
+			ru: "Контакты",
+			en: "Contacts",
+		},
+	},
+	// hero: {
+	// 	name: {
+	// 		ru: "Седрак",
+	// 		en: "Sedrak"
+	// 	},
+	// 	lastname: {
+	// 		ru: "Акобджанян",
+	// 		en: "Hakobjanyan"
+	// 	},
+	// 	text: {
+	// 		ru: "Фул стек разработчик, 43 года, Ереван",
+	// 		en: "Full Stack Developer, 43 years old, Yerevan"
+	// 	},
+	// },
+
+	// about: {
+	// 	title: {
+	// 		ru: "Обо мне",
+	// 		en: "Aboute me"
+	// 	},
+	// 	text1: {
+	// 		ru: "Увлеченный Фул стек разработчик с сильным набором технических и нетехнических навыков и стремлением создавать полезные веб-сайты.",
+	// 		en: "An enthusiastic Full Stack Developer with a strong set of technical as well as non-technical skills and a dedication towards creating useful web sites."
+	// 	},
+	// 	text2: {
+	// 		ru: "Я учусь на курсах «Full Stack Developer» в «WELCOME TO IT».",
+	// 		en: "I'm studying at courses 'Full Stack Developer' in 'WELCOME TO IT'."
+	// 	},
+	// 	text3: {
+	// 		ru: "Готов реализовать отличные проекты с замечательными людьми.",
+	// 		en: "Ready to implement exellent projects with wonderful people."
+	// 	},
+	// },
+
+	skills: {
+		title: {
+			ru: "Навыки",
+			en: "Skills",
+		},
+		text: {
+			ru: "Я работаю в таких программах как",
+			en: "I work in such programs as",
+		},
+	},
+
+	portfolio: {
+		title: {
+			ru: "Портфолио",
+			en: "Portfolio",
+		},
+		descr1: {
+			ru: "Интернет-магазин электроники - Главная страница",
+			en: "Online electronic store - Homepage",
+		},
+		descr2: {
+			ru: "YX - Домашняя страница",
+			en: "YX - Homepage",
+		},
+		descr3: {
+			ru: "Швейцария - Домашняя страница",
+			en: "Switzerland - Homepage",
+		},
+		descr4: {
+			ru: "Целевая страница Braun - концепция",
+			en: "Braun Landing Page - Concept",
+		},
+		descr5: {
+			ru: "Магазин Reebok - концепция",
+			en: "Reebok Store - Concept",
+		},
+		descr6: {
+			ru: "Целевая страница Braun - концепция",
+			en: "Braun Landing Page - Concept",
+		},
+	},
+
+	contacts: {
+		title: {
+			ru: "Контакты",
+			en: "Contacts",
+		},
+		text: {
+			ru: "Хотите узнать больше или просто пообщаться? Пожалуйста!",
+			en: "Want to know more or just chat? You are welcome!",
+		},
+		button: {
+			ru: "Отправить сообщение",
+			en: "Send Message",
+		},
+	},
+
+	footer: {
+		text: {
+			ru: "Поставьте мне лайк в Facebook, LinkedIn, Instagram, Youtube",
+			en: "Like me on Facebook, LinkedIn, Instagram, Youtube",
+		},
+	},
+
+	popup: {
+		buttonClose: {
+			ru: "Закрыть",
+			en: "Close",
+		},
+		name: {
+			ru: "Имя",
+			en: "Name",
+		},
+		email: {
+			ru: "Почта",
+			en: "Email",
+		},
+		phone: {
+			ru: "Телефон",
+			en: "Phone",
+		},
+		message: {
+			ru: "Сообщение",
+			en: "Message",
+		},
+		buttonSend: {
+			ru: "Отправить",
+			en: "Send",
+		},
+	},
+};
